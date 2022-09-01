e15e89db5e604694f6fd0de7a719c03f9d45e899
ManoharPatil98 <53604372+ManoharPatil98@users.noreply.github.com>
2022-09-01 15:42:52 +0530

    Update GitDemo.java

--- qspautomation/src/GitDemo.java
+++ qspautomation/src/GitDemo.java
@@ -3,6 +3,8 @@ public class GitDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Pull Demo: Pull means editing program in github and getting it back on local system");
+		
 
 	}
 
